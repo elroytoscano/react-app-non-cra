1b3199b2d83070312b14aa3f985cd08910e14f14
elroytoscano <andeltos@gmail.com>
2022-03-21 12:53:59 +0530

    initialization

---
+++ app/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import ReactDOM from 'react-dom';
+
+import './index.css';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello </h1>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('app'));
